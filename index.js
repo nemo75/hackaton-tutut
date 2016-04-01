c2b46ad619920b14313f1d8e1cf635e448703f79
Thomas Bricquet <thomas.bricquet@gmail.com>
2016-04-01 17:35:00 +0200

    modification server

--- index.js
+++ index.js
@@ -14,22 +14,21 @@ io.sockets.on('connection', function(socket) {
 
 
 	for(var k in messages_driver) {
-		socket.emit('msg_livre', messages_driver[k]);
+		socket.emit('msg_driver', messages_driver[k]);
 	}
 	for(var k in messages_livre) {
-		socket.emit('msg_driver', messages_livre[k]);
+		socket.emit('msg_livre', messages_livre[k]);
 	}
 
 	socket.on('msg_livre', function(message){
 		me = message;
-		me.id = message.id;
 		socket.emit('send');
+		messages_livre.push(me);
 		io.sockets.emit('msg_livre', me);
 	});
 
 	socket.on('msg_driver', function(message){
 		me = message;
-		me.id = message.id;
 		socket.emit('send');
 		io.sockets.emit('msg_driver', me);
 	});
