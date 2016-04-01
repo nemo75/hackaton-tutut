5d1e91214855f60bfcdf336648cd2771db2fcb8b
Thomas Bricquet <thomas.bricquet@gmail.com>
2016-04-01 11:59:52 +0200

    structure du site ameliore, ajout des scripts et test de la coordination de ces derniers

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+
+app.use(express.static(__dirname + '/public'));
+server.listen(3000);
+
