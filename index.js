a25b586ed63628b3f64ab46c6b1675c5c2370c18
Thomas Bricquet <thomas.bricquet@gmail.com>
2016-04-02 12:46:00 +0200

    rush de folie derniere modifs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var io = require('socket.io').listen(server);
 
 
 app.use(express.static(__dirname + '/public'));
-server.listen(8000);
+server.listen(3000);
 
 io.sockets.on('connection', function(socket) {
 
